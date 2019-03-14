9b02418c9e38fe5f86891b00f0380d339a2bb9cd
Jeff Mitchell <sportcoachnz@gmail.com>
2019-03-14 20:19:08 +1300

    refactoring: Ordered functions alphabetically

--- convertExpenses.py
+++ convertExpenses.py
@@ -13,69 +13,45 @@ total_weekly, total_fortnightly, total_monthly, total_annually = 0, 0, 0, 0
 start_frequency = 0
 start_amount = 0
 
-# convert weekly into annual expense
-def weekly_to_annual(weekly):
-    local_annual = weekly * 52
-    return local_annual
 
-# convert fortnightly into annual expense
-def fortnightly_to_annual(fortnightly):
-    local_annual = fortnightly * 26
-    return local_annual
+def convert_fortnightly(annually):
+    local_fortnightly = annually / 26
+    return local_fortnightly
 
-# convert monthly into annual expense
-def monthly_to_annual(monthly):
-    local_annual = monthly * 12
-    return local_annual
+
+def convert_monthly(annually):
+    local_monthly = annually / 12
+    return local_monthly
+	
 
 # convert annual expenses into weekly, fortnightly and monthly amounts
 def convert_weekly(annually):
     local_weekly = annually / 52
     return local_weekly
 
-def convert_fortnightly(annually):
-    local_fortnightly = annually / 26
-    return local_fortnightly
 
-def convert_monthly(annually):
-    local_monthly = annually / 12
-    return local_monthly
+# display converted amounts
+def display_converted_amounts(weekly, fortnightly, monthly, annually):
+    print("\nA starting amount of ${:.2f} converts as follows:" .format(start_amount))
+    print("\nWeekly it is: ${:.2f}" .format(weekly))
+    print("Fortnightly it is: ${:.2f}" .format(fortnightly))
+    print("Monthly it is: ${:.2f}" .format(monthly))
+    print("Annually it is: ${:.2f}" .format(annually))
 
-# reset totals to zero
-def reset_totals():
-    local_weekly = 0
-    local_fortnightly = 0
-    local_monthly = 0
-    local_annual = 0
-    return local_weekly, local_fortnightly, local_monthly, local_annual
 
-# get and assign frequency selected by user
-def user_frequency():
-    print("""\nPlease enter a number to select a frequency for the expense from the following:\n
-1. Weekly
-2. Fortnightly
-3. Monthly
-4. Annual\n""")
-    correct_input = False
-    while correct_input == False:
-        local_frequency = int(input("Please type a number between 1 and 4 for the frequency of the expense: "))
-        if local_frequency > 0 and local_frequency < 5:
-            correct_input = True
-        else:
-            print("\nThat is not a valid option. Please enter a number between 1 and 4.\n")
-    return local_frequency
+# display total amounts
+def display_total_amounts(weekly, fortnightly, monthly, annually):
+    print("\nTotal weekly expenses are: ${:.2f}" .format(weekly))
+    print("Total fortnightly expenses are: ${:.2f}" .format(fortnightly))
+    print("Total monthly expenses are: ${:.2f}" .format(monthly))
+    print("Total annual expenses are: ${:.2f}" .format(annually))
+	
+	
+# convert fortnightly into annual expense
+def fortnightly_to_annual(fortnightly):
+    local_annual = fortnightly * 26
+    return local_annual
 
-# get and assign starting amount from user
-def get_start_amount():
-    while True:
-        try:
-            local_start_amount = float(input("\nWhat is your starting amount? "))
-        except ValueError:
-            print("\nSorry that is not a valid starting amount. Please enter a number.")
-            continue
-        else:
-            break
-    return local_start_amount
 
 # make calculations by calling functions based on frequency.
 # expenses are converted to an annual amount
@@ -91,60 +67,18 @@ def frequency_conversion(frequency, start_amount):
         local_annual = start_amount
     return local_annual
 
-# present options to user
-def options_list():
-    print("""
-What would you like to do? Please enter a number:\n
-1. Convert an expense
-2. Show current expense totals
-3. Reset the current expense totals
-4. Quit\n""")
-    
-# get user selection
-def user_selection():
-    correct_input = False
-    while correct_input == False:
-        while True:
-            try:
-                local_selection = int(input("Please make a selection: "))
-            except ValueError:
-                print("\nSorry, that is not a valid selection. Please enter a number between 1 and 4.\n")
-                continue
-            else:
-                break
 
-        if local_selection > 0 and local_selection < 5:
-            correct_input = True
+# get and assign starting amount from user
+def get_start_amount():
+    while True:
+        try:
+            local_start_amount = float(input("\nWhat is your starting amount? "))
+        except ValueError:
+            print("\nSorry that is not a valid starting amount. Please enter a number.")
+            continue
         else:
-            print("\nYou must enter a number betweeen 1 and 4\n")
-        
-    # return selection
-    return local_selection
-
-# display converted amounts
-def display_converted_amounts(weekly, fortnightly, monthly, annually):
-    print("\nA starting amount of ${:.2f} converts as follows:" .format(start_amount))
-    print("\nWeekly it is: ${:.2f}" .format(weekly))
-    print("Fortnightly it is: ${:.2f}" .format(fortnightly))
-    print("Monthly it is: ${:.2f}" .format(monthly))
-    print("Annually it is: ${:.2f}" .format(annually))        
-
-# display total amounts
-def display_total_amounts(weekly, fortnightly, monthly, annually):
-    print("\nTotal weekly expenses are: ${:.2f}" .format(weekly))
-    print("Total fortnightly expenses are: ${:.2f}" .format(fortnightly))
-    print("Total monthly expenses are: ${:.2f}" .format(monthly))
-    print("Total annual expenses are: ${:.2f}" .format(annually))
-    
-# see if user wishes to repeat
-def user_repeat():
-    # y/n and check if neither selected
-    local_repeat = input("\nDo you want to continue with another amount or selection? (y/n): ")
-    local_repeat = local_repeat.lower()
-    if local_repeat != "y" and local_repeat != "n":
-        print("\nSorry, that is not a valid selection. Please either 'y' or 'n'.")
-        local_repeat = user_repeat()        
-    return local_repeat
+            break
+    return local_start_amount
 
 
 def main():
@@ -197,7 +131,88 @@ def main():
 
 	print("\nOk, goodbye.")
 	input("\n\nPress the enter key to exit.")
-        
 
+
+# convert monthly into annual expense
+def monthly_to_annual(monthly):
+    local_annual = monthly * 12
+    return local_annual
+
+
+# present options to user
+def options_list():
+    print("""
+What would you like to do? Please enter a number:\n
+1. Convert an expense
+2. Show current expense totals
+3. Reset the current expense totals
+4. Quit\n""")
+
+
+# reset totals to zero
+def reset_totals():
+    local_weekly = 0
+    local_fortnightly = 0
+    local_monthly = 0
+    local_annual = 0
+    return local_weekly, local_fortnightly, local_monthly, local_annual
+
+
+# get and assign frequency selected by user
+def user_frequency():
+    print("""\nPlease enter a number to select a frequency for the expense from the following:\n
+1. Weekly
+2. Fortnightly
+3. Monthly
+4. Annual\n""")
+    correct_input = False
+    while correct_input == False:
+        local_frequency = int(input("Please type a number between 1 and 4 for the frequency of the expense: "))
+        if local_frequency > 0 and local_frequency < 5:
+            correct_input = True
+        else:
+            print("\nThat is not a valid option. Please enter a number between 1 and 4.\n")
+    return local_frequency
+
+
+# see if user wishes to repeat
+def user_repeat():
+    # y/n and check if neither selected
+    local_repeat = input("\nDo you want to continue with another amount or selection? (y/n): ")
+    local_repeat = local_repeat.lower()
+    if local_repeat != "y" and local_repeat != "n":
+        print("\nSorry, that is not a valid selection. Please either 'y' or 'n'.")
+        local_repeat = user_repeat()        
+    return local_repeat
+	
+	
+# get user selection
+def user_selection():
+    correct_input = False
+    while correct_input == False:
+        while True:
+            try:
+                local_selection = int(input("Please make a selection: "))
+            except ValueError:
+                print("\nSorry, that is not a valid selection. Please enter a number between 1 and 4.\n")
+                continue
+            else:
+                break
+
+        if local_selection > 0 and local_selection < 5:
+            correct_input = True
+        else:
+            print("\nYou must enter a number betweeen 1 and 4\n")
+        
+    # return selection
+    return local_selection
+
+
+# convert weekly into annual expense
+def weekly_to_annual(weekly):
+    local_annual = weekly * 52
+    return local_annual
+	
+	
 if __name__ == '__main__':
     main()        
